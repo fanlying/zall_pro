8bfe7d093f431ce0af01f10c92543a911347c468
zhangfan <87023468@qq.com>
2017-08-17 18:16:32 +0800

    提交1

--- Test.py
+++ Test.py
@@ -2,3 +2,17 @@ print ("hgjhlkmklmghge")
 
 a=67
 print (a)
+
+list = ['abcd', 786, 2.23, 'runoob', 70.2]
+tinylist = [123, 'runoob']
+
+print(list)  # 输出完整列表
+print(list[0])  # 输出列表第一个元素
+print(list[1:4])  # 从第二个开始输出到第三个元素
+print(list[2:])  # 输出从第三个元素开始的所有元素
+print(tinylist * 2)  # 输出两次列表
+print(list + tinylist)  # 连接列表
+
+student = {'Tom', 'Jim', 'Mary', 'Tom', 'Jack', 'Rose'}
+
+print(student)  # 输出集合，重复的元素被自动去掉
