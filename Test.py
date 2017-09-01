2232b1b14839e6e10f1956de8b505f546516f644
zhangfan <87023468@qq.com>
2017-09-01 18:02:10 +0800

    提交1

--- Test.py
+++ Test.py
@@ -16,3 +16,112 @@ print(list + tinylist)  # 连接列表
 student = {'Tom', 'Jim', 'Mary', 'Tom', 'Jack', 'Rose'}
 
 print(student)  # 输出集合，重复的元素被自动去掉
+
+
+
+#while True : print('Hello world')
+
+
+# !/usr/bin/python3
+
+# 类定义
+class people:
+    # 定义基本属性
+    name = ''
+    age = 0
+    # 定义私有属性,私有属性在类外部无法直接进行访问
+    __weight = 0
+
+    # 定义构造方法
+    def __init__(self, n, a, w):
+        self.name = n
+        self.age = a
+        self.__weight = w
+
+    def speak(self):
+        print("%s 说: 我 %d 岁。" % (self.name, self.age))
+
+
+# 单继承示例
+class student(people):
+    grade = ''
+
+    def __init__(self, n, a, w, g):
+        # 调用父类的构函
+        people.__init__(self, n, a, w)
+        self.grade = g
+
+    # 覆写父类的方法
+    def speak(self):
+        print("%s 说: 我 %d 岁了，我在读 %d 年级" % (self.name, self.age, self.grade))
+
+
+# 另一个类，多重继承之前的准备
+class speaker():
+    topic = ''
+    name = ''
+
+    def __init__(self, n, t):
+        self.name = n
+        self.topic = t
+
+    def speak(self):
+        print("我叫 %s，我是一个演说家，我演讲的主题是 %s" % (self.name, self.topic))
+
+
+# 多重继承
+class sample(speaker, student):
+    a = ''
+
+    def __init__(self, n, a, w, g, t):
+        student.__init__(self, n, a, w, g)
+        speaker.__init__(self, n, t)
+
+
+test = sample("Tim", 25, 80, 4, "Python")
+test.speak()  # 方法名同，默认调用的是在括号中排前地父类的方法
+
+
+# !/usr/bin/python3
+
+class Site:
+    def __init__(self, name, url):
+        self.name = name  # public
+        self.__url = url  # private
+
+    def who(self):
+        print('name  : ', self.name)
+        print('url : ', self.__url)
+
+    def __foo(self):  # 私有方法
+        print('这是私有方法')
+
+    def foo(self):  # 公共方法
+        print('这是公共方法')
+        self.__foo()
+
+
+x = Site('菜鸟教程', 'www.runoob.com')
+x.who()  # 正常输出
+x.foo()  # 正常输出
+#x.__foo()  # 报错
+
+
+
+# !/usr/bin/python3
+
+class Vector:
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+    def __str__(self):
+        return 'Vector (%d, %d)' % (self.a, self.b)
+
+    def __add__(self, other):
+        return Vector(self.a + other.a, self.b + other.b)
+
+
+v1 = Vector(2, 10)
+v2 = Vector(5, -2)
+print(v1 + v2)
